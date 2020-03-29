1154cd5a2b42638bbc94a49047d02c9ab258299c
The Spine <dtorkelson@gmail.com>
2020-03-29 01:45:28 -0400

    feat: process all subtitle files together

--- code/get_subtitle_data.py
+++ code/get_subtitle_data.py
@@ -1,3 +1,4 @@
+from glob import glob
 from os import path
 import chardet
 import pandas as pd
@@ -6,44 +7,43 @@ import re
 DIR_PATH = path.dirname(path.realpath(__file__))
 
 
-def process_subtitles_by_line(fpath, encoding):
-    subtitle_dict = {
-        'subtitle_index': [],
-        'start': [],
-        'end': [],
-        'text': []
-    }
-    with open(fpath, 'r', encoding=encoding) as f:
-        text_accumulator = []  # gathers multi-line subtitles
-        for line in f:
-            index_match = re.match(r'([0-9]+)\n', line)
-            newline_match = re.fullmatch(r'\n', line)
+def process_subtitles_by_line(fhand, sub_dict):
+    """Adds lines to proper dictionary key.
 
-            if index_match:
-                # line is subtitle index
-                index_int = int(index_match.group(1))
-                subtitle_dict['subtitle_index'].append(index_int)
-            elif re.search(r'-->', line):
-                try:
-                    start, end = re.findall(r'([0-9]{2}:[0-9]{2}:[0-9]{1,2},[0-9]{2,3})', line)
-                except ValueError:
-                    print(line)
-                # subtitle_dict['start'].append(strptime(start, '%H:%M:%S,%f'))
-                # subtitle_dict['end'].append(strptime(end, '%H:%M:%S,%f'))
-                subtitle_dict['start'].append(start)
-                subtitle_dict['end'].append(end)
-            elif newline_match:
-                # blank line between subtitles
-                full_subtitle = ' '.join(text_accumulator)
-                subtitle_dict['text'].append(full_subtitle)
-                text_accumulator = []  # reset accumulator
-            else:
-                # text of the dialogue
-                text_accumulator.append(line.strip())
-        # Save the text contents of the last loop
-        full_subtitle = ' '.join(text_accumulator)
-        subtitle_dict['text'].append(full_subtitle)
-    return subtitle_dict
+    Updates in place, returns nothing.
+    """
+    text_accumulator = []  # gathers multi-line subtitles
+    for line in fhand:
+        index_match = re.match(r'([0-9]+)\n', line)
+        newline_match = re.fullmatch(r'\n', line)
+
+        if index_match:
+            # line is subtitle index
+            index_int = int(index_match.group(1))
+            sub_dict['subtitle_index'].append(index_int)
+        elif re.search(r'-->', line):
+            try:
+                start, end = re.findall(r'([0-9]{2}:[0-9]{2}:[0-9]{1,2},[0-9]{2,3})', line)
+            except ValueError:
+                print(line)
+            # subtitle_dict['start'].append(strptime(start, '%H:%M:%S,%f'))
+            # subtitle_dict['end'].append(strptime(end, '%H:%M:%S,%f'))
+            sub_dict['start'].append(start)
+            sub_dict['end'].append(end)
+        elif newline_match:
+            # blank line between subtitles
+            if not text_accumulator:
+                # no text has been gathered since last iteration, skip the store
+                continue
+            full_subtitle = ' '.join(text_accumulator)
+            sub_dict['text'].append(full_subtitle)
+            text_accumulator = []  # reset accumulator
+        else:
+            # text of the dialogue
+            text_accumulator.append(line.strip())
+    # Save the text contents of the last loop
+    full_subtitle = ' '.join(text_accumulator)
+    sub_dict['text'].append(full_subtitle)
 
 
 def convert_time_cols(df):
@@ -67,7 +67,8 @@ def parse_episode_filename(fname):
         ep_dict = {
             'season_num': int(m.group(1)),
             'episode_num': int(m.group(2)),
-            'episode_name': m.group(3)
+            'episode_name': m.group(3),
+            'filename': fname
         }
     else:
         raise ValueError()
@@ -80,28 +81,48 @@ def add_episode_data(df, ep_dict):
     return df
 
 
-def main():
-    filename = 'Westworld - 2x01 - Journey Into Night.WEB.DEFLATE.en.srt'
-    fpath = path.join(DIR_PATH, '..', 'subtitles', filename)
-    # Detect proper encoding
+def detect_file_encoding(fpath):
+    """For S02E01, it's ISO-8859-1"""
     with open(fpath, 'rb') as rawdata:
         result = chardet.detect(rawdata.read(100000))
-        # For S02E01, it's ISO-8859-1
+    return result['encoding']
 
+
+def all_file_actions(fpath):
+    """Creates dataframe for single episode."""
     # Parse episode name and number
     episode_dict = parse_episode_filename(path.basename(fpath))
 
-    sub_dict = process_subtitles_by_line(fpath, result['encoding'])
-    df_data = pd.DataFrame.from_dict(sub_dict)
+    subtitle_dict = {
+        'subtitle_index': [],
+        'start': [],
+        'end': [],
+        'text': []
+    }
+
+    encoding_guess = detect_file_encoding(fpath)
+    with open(fpath, 'r', encoding=encoding_guess) as f:
+        process_subtitles_by_line(f, subtitle_dict)
+    df_data = pd.DataFrame.from_dict(subtitle_dict)
     df_data = convert_time_cols(df_data)
     df_data = drop_bad_rows(df_data)
     df_data = add_episode_data(df_data, episode_dict)
-    print(df_data.head())
+    return df_data
+
+
+def main():
+    df_all = pd.DataFrame()
+    subtitle_path = path.join(DIR_PATH, '..', 'subtitles', '*.srt')
+    for filepath in glob(subtitle_path):
+        print(filepath)
+        #fpath = path.join(DIR_PATH, '..', 'subtitles', filename)
+        df_episode = all_file_actions(filepath)
+        df_all = df_all.append(df_episode)
 
     # Write out
-    outname = 'S{season_num}E{episode_num:02}_subtitles.csv'.format(**episode_dict)
+    outname = 'subtitle_data.csv'
     outpath = path.join(DIR_PATH, '..', 'data', outname)
-    df_data.to_csv(outpath, index=False, encoding='utf-8')
+    df_all.to_csv(outpath, index=False, encoding='utf-8')
     print(f'Subtitle data written to {outpath}')
 
 
